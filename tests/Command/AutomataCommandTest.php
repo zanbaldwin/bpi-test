1d4990746203191e3ba9f5da17b6a575510f1b5a
Zan Baldwin <hello@zanbaldwin.com>
2020-02-10 16:13:01 -0800

    Create Application Command TestCase Shell

---
+++ tests/Command/AutomataCommandTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace BpiTests\Command;
+
+use BpiTest\Command\AutomataCommand;
+use PHPUnit\Framework\TestCase;
+
+class AutomataCommandTest extends TestCase
+{
+    // ...
+}
