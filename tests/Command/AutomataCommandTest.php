b82779ad4c13fd523f527dfe7a33698b82f3f5b0
Zan Baldwin <hello@zanbaldwin.com>
2020-02-10 17:11:40 -0800

    Add Test Data to Unit Test for Command

--- tests/Command/AutomataCommandTest.php
+++ tests/Command/AutomataCommandTest.php
@@ -7,5 +7,30 @@ use PHPUnit\Framework\TestCase;
 
 class AutomataCommandTest extends TestCase
 {
-    // ...
+    /** @var \BpiTest\Command\AutomataCommand $command */
+    private $command;
+
+    public function setUp(): void
+    {
+        $this->command = new AutomataCommand;
+    }
+
+    public function getTestInputStrings(): array
+    {
+        return [
+            // From requirements document.
+            ['110', 0],
+            ['1010', 1],
+            // Simple numbers.
+            ['0', 0],
+            ['1', 1],
+            ['10', 2],
+            // 1. Mash keypad on calculator with hand.
+            // 2. mod 3
+            // 3. Convert number to binary (using Google because I'm lazy).
+            ['1110000111000111110111001100', 2],
+            ['101000001110000010010001010111111001110100', 1],
+            ['1110100111011001001101100001001000101100101', 0],
+        ];
+    }
 }
